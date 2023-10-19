1a50fd4869b50b7a20aaa5ffc0b76bbf7282ca9c
Dominik <dominikreszczynski10@gmail.com>
2023-10-19 15:30:38 +0200

    css fight

--- src/layout/footer/footer.tsx
+++ src/layout/footer/footer.tsx
@@ -1,16 +1,19 @@
 import "./footer.css";
 import Logo from "../../assets/PokeLab_Logo.png";
+import { Link } from "react-router-dom";
 export const Footer = () => {
   return (
     <footer className="footer">
-      <ul>
-        <li>KONTAKT</li>
-        <li>|</li>
-        <li>
-          <img src={Logo} alt="" />
+      <ul className="footer_ul">
+        <li className="footer_li">KONTAKT</li>
+        <li className="footer_li">|</li>
+        <li className="footer_li">
+          <Link to="/">
+            <img src={Logo} alt="" />
+          </Link>
         </li>
-        <li>|</li>
-        <li>TWÓRCY</li>
+        <li className="footer_li">|</li>
+        <li className="footer_li">TWÓRCY</li>
       </ul>
     </footer>
   );
