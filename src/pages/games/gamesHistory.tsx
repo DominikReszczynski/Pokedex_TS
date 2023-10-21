58665bba8a691c10e564b99a5c1c52a18f9f30f7
Dominik <dominikreszczynski10@gmail.com>
2023-10-21 16:11:26 +0200

    add half game history

---
+++ src/pages/games/gamesHistory.tsx
@@ -0,0 +1,590 @@
+export const mainGames = [
+  {
+    title: "Pocket Monsters Red and Green",
+    systems: [
+      { realaseDate: "1996", system: "Game Boy" },
+      { realaseDate: "2016", system: "3DS Virtual Console" },
+    ],
+    regionRealase: {
+      japan: "February 27, 1996",
+    },
+  },
+  {
+    title: "Pocket Monsters Blue (Pokémon Red and Blue)",
+    systems: [
+      { releaseDate: "1996", system: "Game Boy (CoroCoro Comic)" },
+      { releaseDate: "1999", system: "Game Boy (retail)" },
+      { releaseDate: "2016", system: "3DS Virtual Console" },
+    ],
+    regionRelease: {
+      japan: "October 10, 1999 (retail)",
+      northAmerica: "September 28, 1998",
+      australasia: "October 23, 1998",
+      europ: "October 5, 1999",
+    },
+  },
+  {
+    title: "Pokémon Yellow: Special Pikachu Edition",
+    systems: [
+      { releaseDate: "1998", system: "Game Boy" },
+      { releaseDate: "2016", system: "3DS Virtual Console" },
+    ],
+    regionRelease: {
+      japan: "September 12, 1998",
+      northAmerica: "October 18, 1999",
+      australasia: "September 3, 1999",
+      europe: "June 16, 2000",
+    },
+  },
+  {
+    title: "Pokémon Gold and Silver",
+    systems: [
+      { releaseDate: "1999", system: "Game Boy Color" },
+      { releaseDate: "2017", system: "3DS Virtual Console" },
+    ],
+    regionRelease: {
+      japan: "November 21, 1999",
+      australasia: "October 13, 2000",
+      northAmerica: "October 15, 2000",
+      europe: "April 6, 2001",
+    },
+  },
+  {
+    title: "Pokémon Crystal",
+    systems: [
+      { releaseDate: "2000", system: "Game Boy Color" },
+      { releaseDate: "2018", system: "3DS Virtual Console" },
+    ],
+    regionRelease: {
+      japan: "December 14, 2000",
+      northAmerica: "July 29, 2001",
+      australasia: "September 30, 2001",
+      europe: "November 2, 2001",
+    },
+  },
+  {
+    title: "Pokémon Ruby and Sapphire",
+    systems: [{ releaseDate: "2002", system: "Game Boy Advance" }],
+    regionRelease: {
+      japan: "November 21, 2002",
+      northAmerica: "March 19, 2003",
+      australasia: "April 3, 2003",
+      europe: "July 25, 2003",
+    },
+  },
+  {
+    title: "Pokémon FireRed and LeafGreen",
+    systems: [{ releaseDate: "2004", system: "Game Boy Advance" }],
+    regionRelease: {
+      japan: "January 29, 2004",
+      northAmerica: "September 9, 2004",
+      australasia: "September 23, 2004",
+      europe: "October 1, 2004",
+    },
+  },
+  {
+    title: "Pokémon Emerald",
+    systems: [{ releaseDate: "2004", system: "Game Boy Advance" }],
+    regionRelease: {
+      japan: "September 16, 2004",
+      northAmerica: "May 1, 2005",
+      australasia: "June 9, 2005",
+      europe: "October 21, 2005",
+    },
+  },
+  {
+    title: "Pokémon Diamond and Pearl",
+    systems: [{ releaseDate: "2006", system: "Nintendo DS" }],
+    regionRelease: {
+      japan: "September 28, 2006",
+      northAmerica: "April 22, 2007",
+      australasia: "June 21, 2007",
+      europe: "July 27, 2007",
+    },
+  },
+  {
+    title: "Pokémon Platinum",
+    systems: [{ releaseDate: "2008", system: "Nintendo DS" }],
+    regionRelease: {
+      japan: "September 13, 2008",
+      northAmerica: "March 22, 2009",
+      europe: "May 22, 2009",
+      australasia: "May 14, 2009",
+    },
+  },
+  {
+    title: "Pokémon HeartGold and SoulSilver",
+    systems: [{ releaseDate: "2009", system: "Nintendo DS" }],
+    regionRelease: {
+      japan: "September 12, 2009",
+      northAmerica: "March 14, 2010",
+      australasia: "March 25, 2010",
+      europe: "March 26, 2010",
+    },
+  },
+  {
+    title: "Pokémon Black and White",
+    systems: [{ releaseDate: "2010", system: "Nintendo DS" }],
+    regionRelease: {
+      japan: "September 18, 2010",
+      europe: "March 4, 2011",
+      northAmerica: "March 6, 2011",
+      australasia: "March 10, 2011",
+    },
+  },
+  {
+    title: "Pokémon Black 2 and White 2",
+    systems: [{ releaseDate: "2012", system: "Nintendo DS" }],
+    regionRelease: {
+      japan: "June 23, 2012",
+      northAmerica: "October 7, 2012",
+      australasia: "October 11, 2012",
+      europe: "October 12, 2012",
+    },
+  },
+  {
+    title: "Pokémon X and Y",
+    systems: [{ releaseDate: "2013", system: "Nintendo 3DS" }],
+    regionRelease: {
+      WW: "October 12, 2013",
+    },
+  },
+  {
+    title: "Pokémon Omega Ruby and Alpha Sapphire",
+    systems: [{ releaseDate: "2014", system: "Nintendo 3DS" }],
+    regionRelease: {
+      japan: "November 21, 2014",
+      northAmerica: "November 21, 2014",
+      australasia: "November 21, 2014",
+      europe: "November 28, 2014",
+    },
+  },
+  {
+    title: "Pokémon Sun and Moon",
+    systems: [{ releaseDate: "2016", system: "Nintendo 3DS" }],
+    regionRelease: {
+      japan: "November 18, 2016",
+      northAmerica: "November 18, 2016",
+      australasia: "November 18, 2016",
+      europe: "November 23, 2016",
+    },
+  },
+  {
+    title: "Pokémon Ultra Sun and Ultra Moon",
+    systems: [{ releaseDate: "2017", system: "Nintendo 3DS" }],
+    regionRelease: {
+      WW: "November 17, 2017",
+    },
+  },
+  {
+    title: "Pokémon: Let's Go, Pikachu! and Let's Go, Eevee!",
+    systems: [{ releaseDate: "2018", system: "Nintendo Switch" }],
+    regionRelease: {
+      WW: "November 16, 2018",
+    },
+  },
+  {
+    title: "Pokémon Sword and Shield",
+    systems: [{ releaseDate: "2019", system: "Nintendo Switch" }],
+    regionRelease: {
+      WW: "November 15, 2019",
+    },
+  },
+  {
+    title: "Pokémon Brilliant Diamond and Shining Pearl",
+    systems: [{ releaseDate: "2021", system: "Nintendo Switch" }],
+    regionRelease: {
+      WW: "November 19, 2021",
+    },
+  },
+  {
+    title: "Pokémon Legends: Arceus",
+    systems: [{ releaseDate: "2022", system: "Nintendo Switch" }],
+    regionRelease: {
+      WW: "January 28, 2022",
+    },
+  },
+  {
+    title: "Pokémon Scarlet and Violet",
+    systems: [{ releaseDate: "2022", system: "Nintendo Switch" }],
+    regionRelease: {
+      WW: "November 18, 2022",
+    },
+  },
+];
+
+// ------------------------------------------------------------------------------------------
+
+export const sideGameChronicles = [
+  {
+    title: "Pocket Monsters Stadium",
+    systems: [{ releaseDate: "1998", system: "Nintendo 64" }],
+    regionRelease: {
+      japan: "August 1, 1998",
+    },
+  },
+  {
+    title: "Pokémon Stadium",
+    systems: [{ releaseDate: "1999", system: "Nintendo 64" }],
+    regionRelease: {
+      japan: "April 30, 1999",
+      northAmerica: "February 29, 2000",
+      australasia: "March 23, 2000",
+      europe: "April 7, 2000",
+    },
+  },
+  {
+    title: "Pokémon Stadium 2",
+    systems: [{ releaseDate: "2000", system: "Nintendo 64" }],
+    regionRelease: {
+      japan: "December 14, 2000",
+      northAmerica: "March 28, 2001",
+      europe: "October 10, 2001",
+    },
+  },
+];
+export const otherSideGames = [
+  {
+    title: "Pokémon Box: Ruby and Sapphire",
+    systems: [{ releaseDate: "2003", system: "GameCube" }],
+    regionRelease: {
+      japan: "May 30, 2003",
+      northAmerica: "July 12, 2004",
+      europe: "May 14, 2004",
+      australasia: "July 16, 2004",
+    },
+  },
+  {
+    title: "Pokémon Colosseum",
+    systems: [{ releaseDate: "2003", system: "GameCube" }],
+    regionRelease: {
+      japan: "November 21, 2003",
+      northAmerica: "March 22, 2004",
+      europe: "May 14, 2004",
+    },
+  },
+  {
+    title: "Pokémon XD: Gale of Darkness",
+    systems: [{ releaseDate: "2005", system: "GameCube" }],
+    regionRelease: {
+      japan: "August 4, 2005",
+      northAmerica: "October 3, 2005",
+      australasia: "November 10, 2005",
+      europe: "November 18, 2005",
+    },
+  },
+  {
+    title: "Pokémon Battle Revolution",
+    systems: [{ releaseDate: "2006", system: "Wii" }],
+    regionRelease: {
+      japan: "December 14, 2006",
+      northAmerica: "June 25, 2007",
+      australasia: "November 22, 2007",
+      europe: "December 7, 2007",
+    },
+  },
+  {
+    title: "My Pokémon Ranch",
+    systems: [{ releaseDate: "2008", system: "WiiWare" }],
+    regionRelease: {
+      japan: "March 28, 2008",
+      northAmerica: "June 9, 2008",
+      europe: "July 4, 2008",
+      australasia: "July 4, 2008",
+    },
+  },
+];
+
+// ------------------------------------------------------------------
+
+export const spinOffTCGGames = [
+  {
+    title: "Pokémon Trading Card Game",
+    systems: [
+      { releaseDate: "1998", system: "Game Boy Color" },
+      { releaseDate: "2014", system: "3DS Virtual Console" },
+    ],
+    regionRelease: {
+      japan: "December 18, 1998",
+      northAmerica: "April 10, 2000",
+      europe: "December 15, 2000",
+      australasia: "July 11, 2014",
+    },
+  },
+  {
+    title: "Pokémon Card GB2: Here Comes Great Team Rocket!",
+    systems: [{ releaseDate: "2001", system: "Game Boy Color" }],
+    regionRelease: {
+      japan: "March 28, 2001",
+    },
+  },
+  {
+    title: "Pokémon Play It!",
+    systems: [{ releaseDate: "1999", system: "Windows" }],
+    regionRelease: {
+      europe: "December 1999",
+      northAmerica: "February 2000",
+    },
+  },
+  {
+    title: "Pokémon Play It! Version 2",
+    systems: [{ releaseDate: "2000", system: "Windows" }],
+    regionRelease: {
+      europe: "February 29, 2000",
+      northAmerica: "February 29, 2000",
+    },
+  },
+  {
+    title: "Pokémon Card Game: How To Play DS",
+    systems: [{ releaseDate: "2011", system: "Nintendo DS" }],
+    regionRelease: {
+      japan: "August 5, 2011",
+    },
+  },
+  {
+    title: "Pokémon Trading Card Game Card Dex",
+    systems: [
+      { releaseDate: "2019", system: "iOS" },
+      { releaseDate: "2019", system: "Android" },
+    ],
+    regionRelease: {
+      worldwide: "February 8, 2019",
+    },
+  },
+];
+
+export const spinOffPinballGames = [
+  {
+    title: "Pokémon Pinball",
+    systems: [{ releaseDate: "1999", system: "Game Boy Color" }],
+    regionRelease: {
+      japan: "April 14, 1999",
+      northAmerica: "June 28, 1999",
+      europe: "October 6, 2000",
+      australasia: "July 13, 1999",
+    },
+  },
+  {
+    title: "Pokémon Pinball: Ruby & Sapphire",
+    systems: [
+      { releaseDate: "2003", system: "Game Boy Advance" },
+      { releaseDate: "2015", system: "Wii U Virtual Console" },
+    ],
+    regionRelease: {
+      japan: "August 1, 2003",
+      northAmerica: "August 25, 2003",
+      europe: "November 14, 2003",
+      australasia: "September 26, 2003",
+    },
+  },
+];
+export const spinOffMysteryDungeonGames = [
+  {
+    title: "Pokémon Mystery Dungeon: Blue Rescue Team and Red Rescue Team",
+    systems: [
+      { releaseDate: "2005", system: "Game Boy Advance" },
+      { releaseDate: "2005", system: "Nintendo DS" },
+      { releaseDate: "2016", system: "Wii U Virtual Console" },
+    ],
+    regionRelease: {
+      japan: "November 17, 2005",
+      northAmerica: "September 18, 2006",
+      europe: "November 10, 2006",
+      australasia: "September 28, 2006",
+    },
+  },
+  {
+    title:
+      "Pokémon Mystery Dungeon: Explorers of Time and Explorers of Darkness",
+    systems: [{ releaseDate: "2007", system: "Nintendo DS" }],
+    regionRelease: {
+      japan: "September 13, 2007",
+      northAmerica: "April 20, 2008",
+      europe: "July 4, 2008",
+      australasia: "June 19, 2008",
+    },
+  },
+  {
+    title: "Pokémon Mystery Dungeon: Explorers of Sky",
+    systems: [
+      { releaseDate: "2009", system: "Nintendo DS" },
+      { releaseDate: "2016", system: "Wii U Virtual Console" },
+    ],
+    regionRelease: {
+      japan: "April 18, 2009",
+      northAmerica: "October 12, 2009",
+      europe: "November 20, 2009",
+      australasia: "November 12, 2009",
+    },
+  },
+  {
+    title:
+      "Pokémon Mystery Dungeon: Keep Going! Blazing Adventure Squad!, Let's Go! Stormy Adventure Squad!, and Go For It! Light Adventure Squad!",
+    systems: [{ releaseDate: "2009", system: "WiiWare" }],
+    regionRelease: {
+      japan: "August 4, 2009",
+    },
+  },
+  {
+    title: "Pokémon Mystery Dungeon: Gates to Infinity",
+    systems: [{ releaseDate: "2012", system: "Nintendo 3DS" }],
+    regionRelease: {
+      japan: "November 23, 2012",
+      northAmerica: "March 24, 2013",
+      europe: "May 17, 2013",
+      australasia: "May 18, 2013",
+    },
+  },
+  {
+    title: "Pokémon Super Mystery Dungeon",
+    systems: [{ releaseDate: "2015", system: "Nintendo 3DS" }],
+    regionRelease: {
+      japan: "September 17, 2015",
+      northAmerica: "November 20, 2015",
+      europe: "February 19, 2016",
+      australasia: "February 20, 2016",
+    },
+  },
+  {
+    title: "Pokémon Mystery Dungeon: Rescue Team DX",
+    systems: [{ releaseDate: "2020", system: "Nintendo Switch" }],
+    regionRelease: {
+      worldwide: "March 6, 2020",
+    },
+  },
+];
+export const spinOffRengerGames = [
+  {
+    title: "Pokémon Ranger",
+    systems: [
+      { releaseDate: "2006", system: "Nintendo DS" },
+      { releaseDate: "2016", system: "Wii U Virtual Console" },
+    ],
+    regionRelease: {
+      japan: "March 23, 2006",
+      northAmerica: "October 30, 2006",
+      europe: "April 13, 2007",
+      australasia: "December 7, 2006",
+    },
+  },
+  {
+    title: "Pokémon Ranger: Shadows of Almia",
+    systems: [
+      { releaseDate: "2008", system: "Nintendo DS" },
+      { releaseDate: "2016", system: "Wii U Virtual Console" },
+    ],
+    regionRelease: {
+      japan: "March 20, 2008",
+      northAmerica: "November 10, 2008",
+      europe: "November 21, 2008",
+      australasia: "November 13, 2008",
+    },
+  },
+  {
+    title: "Pokémon Ranger: Guardian Signs",
+    systems: [
+      { releaseDate: "2010", system: "Nintendo DS" },
+      { releaseDate: "2016", system: "Wii U Virtual Console" },
+    ],
+    regionRelease: {
+      japan: "March 6, 2010",
+      northAmerica: "October 4, 2010",
+      europe: "November 5, 2010",
+      australasia: "November 25, 2010",
+    },
+  },
+];
+export const spinOffRumbleGames = [
+  {
+    title: "Pokémon Rumble",
+    systems: [{ releaseDate: "2009", system: "WiiWare" }],
+    regionRelease: {
+      japan: "June 16, 2009",
+      northAmerica: "November 16, 2009",
+      europe: "November 20, 2009",
+    },
+  },
+  {
+    title: "Pokémon Rumble Blast",
+    systems: [{ releaseDate: "2011", system: "Nintendo 3DS" }],
+    regionRelease: {
+      japan: "August 11, 2011",
+      northAmerica: "October 24, 2011",
+      europe: "December 2, 2011",
+    },
+  },
+  {
+    title: "Pokémon Rumble U",
+    systems: [{ releaseDate: "2013", system: "Wii U" }],
+    regionRelease: {
+      japan: "April 24, 2013",
+      pal: "August 15, 2013",
+      northAmerica: "August 29, 2013",
+    },
+  },
+  {
+    title: "Pokémon Rumble World",
+    systems: [{ releaseDate: "2015", system: "Nintendo 3DS" }],
+    regionRelease: {
+      worldwide: "April 8, 2015",
+    },
+  },
+  {
+    title: "Pokémon Rumble Rush",
+    systems: [
+      { releaseDate: "2019", system: "Android" },
+      { releaseDate: "2019", system: "iOS" },
+    ],
+    regionRelease: {
+      australia: "May 15, 2019",
+      worldwide: "May 22, 2019",
+    },
+  },
+];
+export const spinOffSnapGames = [
+  {
+    title: "Pokémon Snap",
+    systems: [
+      { releaseDate: "1999", system: "Nintendo 64" },
+      { releaseDate: "2007", system: "Wii Virtual Console" },
+      { releaseDate: "2017", system: "Wii U Virtual Console" },
+    ],
+    regionRelease: {
+      japan: "March 21, 1999",
+      northAmerica: "June 30, 1999",
+      pal: "September 15, 2000",
+    },
+  },
+  {
+    title: "New Pokémon Snap",
+    systems: [{ releaseDate: "2021", system: "Nintendo Switch" }],
+    regionRelease: {
+      worldwide: "April 30, 2021",
+    },
+  },
+];
+export const spinOffPuzzleGames = [
+  {
+    title: "Pokémon Puzzle League",
+    systems: [
+      { releaseDate: "2000", system: "Nintendo 64" },
+      { releaseDate: "2008", system: "Wii Virtual Console" },
+    ],
+    regionRelease: {
+      northAmerica: "September 25, 2000",
+      europe: "March 16, 2001",
+    },
+  },
+  {
+    title: "Pokémon Puzzle Challenge",
+    systems: [
+      { releaseDate: "2000", system: "Game Boy Color" },
+      { releaseDate: "2014", system: "3DS Virtual Console" },
+    ],
+    regionRelease: {
+      japan: "September 21, 2000",
+      northAmerica: "December 4, 2000",
+      pal: "June 15, 2001",
+    },
+  },
+];
