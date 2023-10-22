f904637613efaa59d22cceda92facfe1f7f34ae6
Dominik <dominikreszczynski10@gmail.com>
2023-10-22 16:07:14 +0200

    axios problem

--- src/pages/pokedex/pokedex.tsx
+++ src/pages/pokedex/pokedex.tsx
@@ -1,7 +1,7 @@
 // @ts-nocheck
 import React, { useEffect, useState } from "react";
 import "./pokedex.css";
-import axios from "axios";
+// import axios from "axios";
 
 import { OnePokemon } from "./onePokemon";
 export function Pokedex() {
@@ -11,16 +11,20 @@ export function Pokedex() {
   const BASE_POKEMON_API_URL = "https://pokeapi.co/api/v2/pokemon?limit=200";
 
   useEffect(() => {
-    axios
-      .get(BASE_POKEMON_API_URL)
+    fetch(BASE_POKEMON_API_URL)
       .then((response) => {
-        const pokemonList = response.data.results;
+        if (!response.ok) {
+          throw new Error("Network response was not ok");
+        }
+        return response.json();
+      })
+      .then((data) => {
+        const pokemonList = data.results;
         const pokemonPromises = pokemonList.map((pokemon) =>
-          axios.get(pokemon.url)
+          fetch(pokemon.url).then((response) => response.json())
         );
         Promise.all(pokemonPromises)
-          .then((responses) => {
-            const pokemonsData = responses.map((response) => response.data);
+          .then((pokemonsData) => {
             console.log(pokemonsData);
             setPokemons(pokemonsData);
             setIsLoading(false);
@@ -35,6 +39,7 @@ export function Pokedex() {
         setIsLoading(false);
       });
   }, []);
+
   return (
     <div>
       <h1>{getIsLoading && <div className="loader" />}</h1>
